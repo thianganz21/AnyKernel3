277aaaf9dde6187d1b999f3546ef7d852130022f
thianganz21 <65148082+thianganz21@users.noreply.github.com>
2025-12-02 20:26:07 +0800

    Refactor AnyKernel3 properties and boot variables
    Updated properties and boot shell variables for AnyKernel3.

--- anykernel.sh
+++ anykernel.sh
@@ -2,31 +2,37 @@
 ## osm0sis @ xda-developers
 
 ### AnyKernel setup
-# global properties
+# begin properties
 properties() { '
-kernel.string=Kernel by ? @ xda-developers
-do.devicecheck=1
+kernel.string=
+do.devicecheck=0
 do.modules=0
-do.systemless=1
+do.systemless=0
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=ginkgo
-supported.versions=11 - 15
+supported.versions=
 supported.patchlevels=
-supported.vendorpatchlevels=
 '; } # end properties
 
+### AnyKernel install
+# begin attributes
+attributes() {
+set_perm_recursive 0 0 755 644 $ramdisk/*;
+set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
+} # end attributes
 
-# boot shell variables
-BLOCK=boot;
-IS_SLOT_DEVICE=auto;
-RAMDISK_COMPRESSION=auto;
+
+## boot shell variables
+block=boot;
+is_slot_device=1;
+ramdisk_compression=auto;
+patch_vbmeta_flag=auto;
 
 # import functions/variables and setup patching - see for reference (DO NOT REMOVE)
-. tools/ak3-core.sh;
+. tools/ak3-core.sh && attributes;
 
 # boot install
-dump_boot;
+dump_boot; # use split_boot to skip ramdisk unpack, e.g. for devices with init_boot ramdisk
 
-write_boot;
+write_boot; # use flash_boot to skip ramdisk repack, e.g. for devices with init_boot ramdisk
 ## end boot install
