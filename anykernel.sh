e285f9f985bcfee8f81c9c0a63000d7643d0cb76
thianganz21 <65148082+thianganz21@users.noreply.github.com>
2025-12-02 08:23:58 +0000

    update README.md

---
+++ anykernel.sh
@@ -0,0 +1,32 @@
+### AnyKernel3 Ramdisk Mod Script
+## osm0sis @ xda-developers
+
+### AnyKernel setup
+# global properties
+properties() { '
+kernel.string=Kernel by ? @ xda-developers
+do.devicecheck=1
+do.modules=0
+do.systemless=1
+do.cleanup=1
+do.cleanuponabort=0
+device.name1=ginkgo
+supported.versions=11 - 15
+supported.patchlevels=
+supported.vendorpatchlevels=
+'; } # end properties
+
+
+# boot shell variables
+BLOCK=boot;
+IS_SLOT_DEVICE=auto;
+RAMDISK_COMPRESSION=auto;
+
+# import functions/variables and setup patching - see for reference (DO NOT REMOVE)
+. tools/ak3-core.sh;
+
+# boot install
+dump_boot;
+
+write_boot;
+## end boot install
